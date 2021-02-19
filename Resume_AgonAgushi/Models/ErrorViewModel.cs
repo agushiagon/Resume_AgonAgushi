507936c3041a077d29b96d394830790534a6238c
Agon Agushi <agonagushi96@gmail.comm>
2021-02-19 10:46:04 +0100

    Add project files.

---
+++ Resume_AgonAgushi/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Resume_AgonAgushi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
